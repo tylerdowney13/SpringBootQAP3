8dce29b90b868c118f9b49d5ef736cb1937beacc
tylerdowney13 <tylerdowney83@gmail.com>
2022-03-28 10:41:36 -0230

    Initial commit

---
+++ SpringBootQAP3/src/main/java/com/tylerdowney/SpringBootQAP3/SpringBootQap3Application.java
@@ -0,0 +1,13 @@
+package com.tylerdowney.SpringBootQAP3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQap3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQap3Application.class, args);
+	}
+
+}
---
+++ SpringBootQAP3/src/test/java/com/tylerdowney/SpringBootQAP3/SpringBootQap3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerdowney.SpringBootQAP3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootQap3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
